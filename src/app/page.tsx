4c7f2ec208c648f2f68707f2f74a5ca7e445ccd0
owbird <owbirdphorccuo@gmail.com>
2024-06-10 21:43:40 +0000

    feat: Improve UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ function formatBytes(bytes: number): string {
   return `${size} ${sizes[i]}`;
 }
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const res = await fetch(`http://localhost:8080/`, { cache: "no-cache" });
@@ -21,16 +21,23 @@ export default async function Home() {
       <div className="flex flex-col items-center">
         <h1 className="text-3xl font-bold mb-4">SVault File Server</h1>
 
-        <div>
+        <div className="grid lg:grid-cols-4 m-4">
           {files.map((file) => (
-            <div key={file.name} className="flex items-center gap-2 rounded-md p-2 hover:bg-teal-100">
+            <div
+              key={file.name}
+              className="flex items-center gap-2 rounded-md p-2 hover:bg-teal-100"
+            >
               {file.is_dir ? (
                 <FaFolder size={30} color={"yellow"} />
               ) : (
                 <FaFile size={30} color={"blue"} />
               )}
               <p>{file.name}</p>
-              <p className="bg-rose-950 text-white pl-2 pr-2 rounded-2xl">{formatBytes(file.size)}</p>
+              { !file.is_dir &&
+                <p className="bg-rose-950 text-white pl-2 pr-2 rounded-2xl">
+                  {formatBytes(file.size)}
+                </p>
+              }
             </div>
           ))}
         </div>
