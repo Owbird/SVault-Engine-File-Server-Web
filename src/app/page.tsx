f084b4170c6d029ed94c0c8ba1e7f3b6c0d45cdb
owbird <owbirdphorccuo@gmail.com>
2024-06-11 23:55:24 +0000

    feat: Navigate dirs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
+import File from "@/components/File";
 import { FaFile, FaFolder } from "react-icons/fa";
-
-function formatBytes(bytes: number): string {
-  if (bytes === 0) return "0 Bytes";
-  const k = 1024;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  const size = parseFloat((bytes / Math.pow(k, i)).toFixed(2));
-  return `${size} ${sizes[i]}`;
-}
 
 export const dynamic = "force-dynamic";
 
@@ -23,22 +15,7 @@ export default async function Home() {
 
         <div className="grid lg:grid-cols-4 m-4">
           {files.map((file) => (
-            <div
-              key={file.name}
-              className="flex items-center gap-2 rounded-md p-2 hover:bg-teal-100"
-            >
-              {file.is_dir ? (
-                <FaFolder size={30} color={"yellow"} />
-              ) : (
-                <FaFile size={30} color={"blue"} />
-              )}
-              <p>{file.name}</p>
-              { !file.is_dir &&
-                <p className="bg-rose-950 text-white pl-2 pr-2 rounded-2xl">
-                  {formatBytes(file.size)}
-                </p>
-              }
-            </div>
+            <File currentDir="/" key={file.name} file={file} />
           ))}
         </div>
       </div>
